255cb5558247e4cc5d2c23623a49a4ecb4de7260
antonfuflygin <anton.fuflygin@gmail.com>
2026-03-13 18:50:46 +0300

    feat: добавил клавиатуру с выбором варианта ответа

--- app/index.js
+++ app/index.js
@@ -4,17 +4,26 @@ const token = "8672001788:AAFfchuzjilMUe5IKMINYIVh9c8uDylForc";
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   const chatId = msg.chat.id;
+//   const resp = match[1];
 
-  bot.sendMessage(chatId, resp);
-});
+//   console.log(resp)
+
+//   bot.sendMessage(chatId, resp);
+// });
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, "Received your message");
+  bot.sendMessage(chatId, "Received your message", {
+    reply_markup: {
+      keyboard: [
+        ["A", "B"],
+        ["C", "D"],
+      ],
+    },
+  });
 });
 
-bot.onText
+bot.onText;
